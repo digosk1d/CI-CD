5ab1988a4d3304ede09ac6c6b016f8a69a2f8449
digoskid <digoskid@gmail.com>
2025-08-19 10:26:37 -0500

    Actualizar test

--- src/utils/conversions.js
+++ src/utils/conversions.js
@@ -1,15 +1,15 @@
 function toCelsius(f) {
-  if (isNaN(f)) throw new Error('No es un número');
+  if (isNaN(f)) throw new Error('No es un numero');
   return Math.round((f - 32) * 5 / 9);
 }
 
 function toFahrenheit(c) {
-  if (isNaN(c)) throw new Error('No es un número');
+  if (isNaN(c)) throw new Error('No es un numero');
   return Math.round((c * 9 / 5) + 32);
 }
 
 function movingAverage(series, window) {
-  if (series.some(x => isNaN(x)) || window < 2) throw new Error('Datos o ventana incorrectos');
+  if (series.some(x => isNaN(x)) || window < 2) throw new Error('Datos incorrectos');
   let result = [];
   for (let i = 0; i <= series.length - window; i++) {
     let sum = 0;
